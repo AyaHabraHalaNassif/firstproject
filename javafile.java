cdc27146c412f22866cbbcff08702ab6c94518fd
Hala Nassif <hala.nassif.96@gmail.com>
2019-03-30 12:29:45 +0300

    Hala modified (javafile.java) file

--- javafile.java
+++ javafile.java
@@ -3,9 +3,10 @@ public class javaFile{
 int x=5;
 public javaFile(){
 super();
-x=13;}
+x=12;}
 public void print(){
 System.out.println("java");
+System.out.println("software enginnering 2");
 }
 
 }
