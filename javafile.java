f43138709878c8c5cc64d2fbb6653ad2f1138e16
Aya Habra <aya.habra.97@gmail.com>
2019-03-30 13:29:23 +0200

    add y,z variables and put sum x and y in z then print z

--- javafile.java
+++ javafile.java
@@ -1,10 +1,15 @@
 public class javaFile{
-
+int y =3;
 int x=5;
+int z;
 public javaFile(){
 super();
-x=12;}
+x=12;
+y=15;
+z=x+y;
+}
 public void print(){
+System.out.println("sum x+y ="+ +z);
 System.out.println("java");
 System.out.println("software enginnering 2");
 }
